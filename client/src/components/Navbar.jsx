f227ab3ccee55e708889cd5c854f78d409c37107
EUGEN-NYONGESA <eugenyongesa@gmail.com>
2025-01-13 16:07:51 +0300

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,42 @@ import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AppContent} from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Navbar = () => {
 
   const navigate = useNavigate()
   const {userData, backendUrl, setUserData, setIsLoggedin} = useContext(AppContent)
+
+  const sendVerificationOtp = async ()=>{
+    try {
+      axios.defaults.withCredentials = true;
+      const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp')
+
+      if(data.success){
+        navigate('/email-verification')
+        toast.success(data.message)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const logout = async ()=> {
+    try {
+      axios.defaults.withCredentials = true
+      const {data} = await axios.post(backendUrl + '/api/auth/logout')
+      data.success && setIsLoggedin(false)
+      data.success && setUserData(false)
+      navigate('/')
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6 
@@ -20,9 +51,12 @@ const Navbar = () => {
         <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
           {userData.name[0].toUpperCase()}
           <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
-            <ul>
-              <li>Verify Email</li>
-              <li>Logout</li>
+            <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
+              {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-300 
+              cursor-pointer'>Verify Email</li>}
+
+              <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 
+              cursor-pointer'>Logout</li>
             </ul>
           </div>
         </div>
